e6b12e08f8960288897f60e5f9693572c980ec35
Rodolfo E. Edelmann <rudicba@gmail.com>
2015-11-01 18:12:58 -0300

    Add folderset for handle folders, add __init__ docstring

--- cutter/lib/cutter.py
+++ cutter/lib/cutter.py
@@ -7,12 +7,34 @@ import subprocess
 
 
 class Cutter(object):
+    """Handle file dependencies
+
+    Note:
+        Cutter will track needed files for running executables inside a chroot environment.
+
+    Attributes:
+        fileset (array set): All files needed for project.
+        folderset (array set): Folders needed for project.
+
+    Examples:
+        >>> cutter = Cutter()
+        >>> cutter.add_file('/bin/ls')
+        >>> cutter.fileset
+        {'/bin/ls',
+         '/lib/x86_64-linux-gnu/ld-2.19.so',
+         '/lib/x86_64-linux-gnu/libacl.so.1',
+         ......
+         '/lib64/ld-linux-x86-64.so.2'}
+         >>> cutter.folderset
+         {'/bin', '/lib/x86_64-linux-gnu', '/lib64'}
+    """
 
     def __init__(self):
         self.fileset = set([])
+        self.folderset = set([])
 
     def add_file(self, path):
-        '''Add file to fileset.
+        """Add file to fileset, update folderset.
 
         Note:
             If file is symlink add symlink detination to fileset.
@@ -24,11 +46,12 @@ class Cutter(object):
         Returns:
             True if successful, False otherwise.
 
-        '''
+        """
         assert os.path.exists(path)
 
         mime = magic.from_file(path, mime=True)
         self.fileset.add(path)
+        self.folderset.add(os.path.dirname(path))
 
         if mime.decode("utf-8") == 'inode/symlink':
             self.add_file(os.path.realpath(path))
@@ -39,7 +62,7 @@ class Cutter(object):
         return True
 
     def add_folder(self, path):
-        '''Add path to fileset.
+        """Add path to fileset.
 
         Note:
             If path is folder add all folder and subfolder files to fileset.
@@ -51,7 +74,7 @@ class Cutter(object):
         Returns:
             True if successful, False otherwise.
 
-        '''
+        """
         assert os.path.isdir(path)
 
         for root, dirnames, filenames in os.walk(path):
@@ -62,7 +85,7 @@ class Cutter(object):
         return True
 
     def add_path(self, path):
-        '''Add path to fileset.
+        """Add path to fileset.
 
         Args:
             path: Path of folder to walk.
@@ -70,7 +93,7 @@ class Cutter(object):
         Returns:
             True if successful, False otherwise.
 
-        '''
+        """
         assert os.path.exists(path)
 
         if os.path.isfile(path):
@@ -79,7 +102,7 @@ class Cutter(object):
             self.add_folder(path)
 
     def ldd(self, path):
-        '''List Dynamic Dependencies.
+        """List Dynamic Dependencies.
 
         Args:
             path: Path of x-executable file.
@@ -87,17 +110,17 @@ class Cutter(object):
         Returns:
             List of all Dynamic Dependencies needed for executable
 
-        '''
+        """
         ldd_out = subprocess.check_output(['ldd', str(path)])
 
         libraries = []
         for line in ldd_out.splitlines():
             match = re.match(r'\t(.*) => (.*) \(0x', line.decode("utf-8"))
             if match:
-                libraries.append(match.group(2))
+                libraries.append(str(match.group(2)))
             else:
                 # TODO: THIS SUCKS
-                libraries.append(line.split()[0].decode("utf-8"))
+                libraries.append(str(line.split()[0].decode("utf-8")))
 
         # TODO: THIS SUCKS
         libraries.remove('')
